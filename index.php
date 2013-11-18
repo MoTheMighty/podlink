0199ca5e5028adfe2462ff01a1e1b9cf9ca020c1
richard.perez <pod1098@richards-iMac.local>
2013-11-18 17:45:25 -0600

    perra

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+      <script src="js/respond.min.js"></script>
    
   </head>
 
