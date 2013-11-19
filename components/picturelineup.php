9a14d455841d2830859bc7e320e64116af2ae47b
richard.perez <pod1098@richards-iMac.local>
2013-11-18 18:24:17 -0600

    hi

--- components/picturelineup.php
+++ components/picturelineup.php
@@ -9,7 +9,7 @@
         <h3 class="text-center">Team Members<h3>
 </div><!-- container -->
 
-<section="container">
+<div="container">
 
     <div class="col col-sm-6 col-md-6 col-lg-3">
     
@@ -67,4 +67,4 @@
    
 </div>
 
-</section> <!-- container -->
+</div> <!-- container -->
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <title>Put you title here</title>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.css" rel="stylesheet">
@@ -12,7 +13,7 @@
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="js/html5shiv.js"></script>
       <script src="js/respond.min.js"></script>
    
   </head>
