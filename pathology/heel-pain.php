bec1d6449879f46aa935daa6de2e72c66ac26604
richard.perez <pod1098@richards-iMac.local>
2013-11-29 17:51:45 -0600

    heelpain

---
+++ pathology/heel-pain.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html
+     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+<html>
+  <head>
+    <title>Plantar Fasciitis - Heel Pain</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../../css/bootstrap.css" rel="stylesheet">
+    <!--<link href="css/font-awesome.min.css" rel="stylesheet">-->
+    <link href="../../css/mystyles.css" rel="stylesheet">
+    <!-- <link href="css/glyphicons.css" rel="stylesheet"> -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    
+      <script src="/js/html5shiv.js"></script>
+      <script src="/js/respond.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+  </head>
+
+  <body id="plantarfasciitis">
+
+  <?php include '../components/navhead.php'; ?>
+
+
+
+  <div class="navibar">
+    <section class="container">
+  
+      <div class="pull-left">
+        <p>Plantar Fasciitis<p>
+      </div><!-- pullleft -->
+      <div class="pull-right">
+        <ol class="breadcrumb">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="../../about.php">Conditions</a></li>
+            <li class="active">Plantar Fasciitis</li>
+        </ol><!-- breadcrumb -->
+      </div><!-- pull-right -->
+     
+    </section><!-- container -->
+  </div><!-- navibar -->
+
+  <!-- end navibar -->
+
+<div class="layoutspacing2"></div>
+<div class="container lead">
+<div itemscope itemtype="http://schema.org/MedicalCondition">
+<div><img alt="" src="/images/pathology/plantar-fasciitis.jpg" style="float: right;" /><br />
+</div>
+<p>The most common reason patients seek our care is for heel pain. This condition can be caused by a variety of factors. Including, entrapped nerves, metabolic disorders, arthritis and infections. By far the most common cause, accounting for 95% of all cases, is abnormal, repetitive stresses placed on the anatomic structures of the foot and leg resulting in injury and inflammation. This condition is known as <span itemprop="name">plantar fasciitis</span>.</p>
+<p>The <span itemprop="associatedAnatomy" itemscope itemtype="http://schema.org/AnatomicalStructure"><span itemprop="name">plantar fascia</span></span> has several functions. It helps keep the small muscles of the foot in a compact and efficient package beneath the foot when full weight is bore. More importantly, it helps maintain the bones of the foot in an "arch" orientation. As body weight is transmitted down through the leg and ankle, the foot tries to maintain a triangle shape. The base of the triangle is the plantar fascia. The fascia must resist the force of the heel and arch bones to flatten out. The extent to which the arch flattens is dependent on how much the plantar fascia can support. Thus, during the normal course of walking, our plantar fascia is subjected to tremendous, repetitive forces.</p>
+<p>To understand what is causing heel pain, an examination of the anatomy is necessary. The plantar fascia is a dense thin layer of tissue just beneath the skin on the sole of the foot. It is shaped like a triangle (blue lines), with the apex oriented towards the back of the foot.</p>
+
+<p><img src="/images/pathology/plantar-fasciitisline.jpg" alt="Anatomic orientation of the plantar fascia" style="float: left; margin-left: 15px; margin-right: 15px;"/></p>
+<p> The plantar fascia has several functions. It helps keep the small muscles of the foot in a compact and efficient package beneath the foot when full weight is bore. More importantly, it helps maintain the bones of the foot in an "arch" orientation.</p>
+<p>As body weight is transmitted down through the leg and ankle, the foot tries to maintain a triangle shape. The base of the triangle is the plantar fascia (blue line). The fascia must resist the force of the heel and arch bones to flatten out(red lines). The extent to which the arch flattens is dependent on how much the plantar fascia can support. Thus, during the normal course of walking, our plantar fascia is subjected to tremendous, repetitive forces.</p>
+
+
+<p><img src="/images/pathology/pfforces.jpg" alt="Forces resisted by the plantar fascia" style="float: right; margin-left: 15px;"  /></p>
+<p> Since the plantar fascia is shaped like a triangle, a great deal of force is concentrated at the heel. When too much strain is placed on the fascia, the area of greatest strain becomes inflamed. This inflammatory process leads to pain and swelling. This is often termed plantar fasciitis or heel spur syndrome. Over time, this condition may produce bone projections off the heel known as spurs. Often times, patients will come to the office with heel pain wishing their "spurs" be removed. They believe the heel pain is caused by these bony growths, when in fact, the true reason for the pain is from inflammation of their plantar fascia. The heel spur has developed in response to the chronic irritation of the plantar fascia on the calcaneus (heel bone). </p>
+<hr>
+<div class="well center">
+
+<h4>"Treatment targets the plantar fascia, not the heel spur, since it is the true source of the  pain."</h4>
+</div>
+<hr>
+<h3>Signs and symptoms</h3>
+
+<p><span itemprop="signOrSymptom" itemscope itemtype="http://schema.org/MedicalSymptom"><span itemprop="name">Patients present with chronic pain to the bottom of the heel. Usually, this pain has been  present for weeks to months and they have delayed seeking treatment hoping the symptoms would subside. Terms such as a stone bruise or deep ache are usually used  to describe the discomfort. The onset is usually gradual, with no recollection of an injury  or accident.</p>
+<p>Classically, the pain is worse when arising from rest. For example, the first few steps  taken after waking in the morning or after a prolonged period of sitting can generate the  most pain. Once the foot warms up, the heel pain may lessen. Swelling or puffiness on  the inside of the heel may also be noticed.</p></span></span>
+
+<p><h3>Treatment:</h3><p>
+<p>For the vast majority of patients (95%), conservative treatment can eliminate their pain. We tailor our treatment to the individual patient, taking into account health, lifestyle and their work environment. A cookie cutter approach will not be as effective, as there are many factors which must be considered. In general, the steps outlines below are utilized in different combinations and varying degrees of emphasis.</p>
+
+<hr>
+
+<p><span class="label label-primary">Anti-inflammatory Medication</span><p>
+<p>Also known as NSAID's, this class of drugs helps to decrease the inflammatory process  within the plantar fascia. We frequently use these drugs for a period of several months.</p>
+<hr>
+
+<p><p><span class="label label-primary">Stretching:</span><p></p>
+<p style="text-align: center;"> <iframe width="300" height="182" frameborder="0" src="http://www.youtube.com/embed/hauyuX-uCq8"></iframe> </p>
+<p>By stretching the plantar fascia, the abnormal pressure can be alleviated and resolution of the problem obtained. Many times night splints can achieve this goal with minimal pain and inconvenience.</p>
+<hr>
+
+<p><span class="label label-primary">Injections:</span><p>
+
+<p>Cortisone injections carry potent anti-inflammatory action. They are used for moderate  to severe cases of plantar fasciitis. Plantar fasciitis which does not respond to other  forms of treatment are also an indication for their use.</p>
+<hr>
+
+<p><span class="label label-primary">Orthoses (arch supports):</span><p>
+<p>These devices help to support the arch and take pressure off the plantar fascia . They  are placed in shoe gear and can help prevent recurrence of the condition. Both custom  molded orthotics and over-the-counter devices can be effective.</p>
+<hr>
+
+<p><span class="label label-primary">Footwear:</span><p>
+<p>Proper fitting and supporting shoe gear is critical is alleviating heel pain. Many times we  place our patients in athletic shoes for prolonged periods. These styles allow more  support to the arch and heel, creating a favorable environment for recovery. </p>
+<hr>
+
+<p><span class="label label-primary">Rest:</span><p>
+<p>In severe cases of heel pain, we ask our patient to take time off work to rest their foot.  This is especially true for people who spend a great deal of time on their feet. This may  be combined with immobilization in a short leg cast.</p>
+<hr>
+
+<p><span class="label label-primary">Surgery:</span><p>
+<p>Surgery maybe needed for those cases of plantar fasciitis which have failed all forms of conservative treatment. There are many different forms of surgery for plantar fasciitis, each of our physicians has his preferred method.</p>
+
+ <p>Click <a href="http://www.youtube.com/watch?v=tUrkGnRdOhs" title="Watch a youtube video ">here </a>to watch a video of this surgery being performed.</p>
+<p><strong>Extracorporeal shock wave treatment (ESWT)</strong></p>
+<p>This technology represent the latest innovation in the treatment of resistant plantar  fasciitis. We have been utilizing this modality since January of 2000 with excellent  results. <a title="Shock Wave Therapy" href="/treatments-and-conditions/shockwave-treatment.aspx">Learn more about Shock Wave Therapy</a></p>
+</div><!-- schema -->
+</div><!-- container -->
+<!-- footer -->
+
+   <?php include '../components/footer.php'; ?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/modernizr.js"></script>
+    <?php include '../../components/social.php'; ?>
+
+
+
+  </body>
+</html>
--- test.php
+++ test.php
@@ -1,13 +1,62 @@
 
-  <?php
-   $message = array(
-        "name" => $_GET["first_name"],
-        "username" => $_GET["last_name"],
-        "token" => "b0b0f25f-c65b-43ce-8df8-cbc5dfbd1a13"
-    ); 
-    ?>
-
-    <?php echo $_POST["first_name"]; ?>
-    <?php print_r($message); ?>
+<!DOCTYPE html
+     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 
+<html>
+  <head>
+    <title>Put you title here</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!--<link href="css/font-awesome.min.css" rel="stylesheet">-->
+    <link href="css/mystyles.css" rel="stylesheet">
+    <!-- <link href="css/glyphicons.css" rel="stylesheet"> -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+  </head>
+<body>
+<p>sadasdsadasd<p>
+
+
+<section class="container">
+<div="row">
+  <section id="bumperpads">
+ <div id="info">
+  <div class="col-lg-4 col-md-4 col-sm-4">
+  <form method="POST" action="<?php include 'php/examples/simple.php'; ?>" >
+    <div class="form-group">
+      <label for="firstname">First Name: </label>
+      <input type="text" name="first_name" class="form-control" id="firstname" placeholder="First Name">
+    </div><!-- formgroup firstname -->
+    <div class="form-group">
+      <label for="lastname">Last Name:</label>
+      <input type="text" name="last_name" class="form-control" id="lastname" placeholder="Last Name">
+    </div><!-- lastname -->
+    <form role="form">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email Address:</label>
+      <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+    </div><!-- email -->
+    <label for="message">Message:</label>
+    <textarea name="contact_message" class="form-control" rows="4"></textarea>
+    
+  <br>
+  
+  <button type="submit" name="submitbutton" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
+</div>
+</div><!-- col -->
+
+</body>
+
+</html>
