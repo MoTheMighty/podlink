4c121e8e12000dd779c02b3641b3b27ebe9ee45b
richard.perez <pod1098@richards-iMac.local>
2013-12-01 18:51:57 -0600

    bunionv1

---
+++ pathology/bunions.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html
+     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+<html>
+  <head>
+    <title>Plantar Fasciitis - Heel Pain</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../../css/bootstrap.css" rel="stylesheet">
+    <!--<link href="css/font-awesome.min.css" rel="stylesheet">-->
+    <link href="../../css/mystyles.css" rel="stylesheet">
+    <!-- <link href="css/glyphicons.css" rel="stylesheet"> -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    
+      <script src="/js/html5shiv.js"></script>
+      <script src="/js/respond.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+  </head>
+
+  <body id="bunions">
+
+  <?php include '../components/navhead.php'; ?>
+
+
+
+  <div class="navibar">
+    <section class="container">
+  
+      <div class="pull-left">
+        <p>Bunions<p>
+      </div><!-- pullleft -->
+      <div class="pull-right">
+        <ol class="breadcrumb">
+            <li><a href="../../index.php">Home</a></li>
+            <li><a href="../../pathology.php">Conditions</a></li>
+            <li class="active">Bunions</li>
+        </ol><!-- breadcrumb -->
+      </div><!-- pull-right -->
+     
+    </section><!-- container -->
+  </div><!-- navibar -->
+
+  <!-- end navibar -->
+<div class="layoutspacing2"></div>
+
+<div class="container">
+ <ul class="nav nav-tabs nav-justified" style="margin: 20px 0px;">
+              <li class="active"><a href="#cause" data-toggle="tab">Cause</a></li>
+              <li><a href="#treatment" data-toggle="tab">Treatment</a></li>
+              <li><a href="#goals" data-toggle="tab">Treatment Goals</a></li>
+              <li><a href="#moderate" data-toggle="tab">Minimal/Moderate Deformity</a></li>
+              <li><a href="#severe" data-toggle="tab">Severe</a></li>
+             
+            </ul>
+
+            <!-- Tab panes -->
+           
+            <div class="tab-content" style="padding: 0px 60px;">
+              <div class="tab-pane fade in active" id="cause">
+                <p>One of the most commonly treated foot conditions are painful bunions. Patients with this condition will usually complain of pain when wearing shoes, especially narrow dress shoes, or with physical activity, such as walking or running.</p>
+                    <div style="text-align: center;"> <iframe width="399" height="203" frameborder="0" src="http://www.youtube.com/embed/xGtSEK5QMoI"></iframe> </div>
+                    <h3><strong>Cause of Bunion Deformity</strong></h3>
+                    <p>The classic bunion is a bump on the side of the great toe joint. This bump represents an actual deviation of the 1st metatarsal. In addition, there is also deviation of the great toe toward the second toe. In severe cases, the great toe can either lie above or below the second toe.</p>
+                    <p style="text-align: center;"><img src="/images/pathology/bunionrotation.jpg" alt="bunion splaying of foot xray with arrows" ></p>
+                    <p>Shoes are often blamed for creating these problems. This, however, is inaccurate. It has been researched in primitive tribes where going barefoot is the norm and they also developed bunions. Bunions develop from abnormal foot structure and mechanics that places an abnormal pressure on the 1st metatarsal. This leads to stretching of supporting soft tissue structures such as joint capsules and ligaments with the end result being gradual deviation of the 1st metatarsal. As the deformity increases, there is an abnormal pull of certain tendons, which leads to the drifting of the great toe toward the 2nd toe. At this stage, there is also adaptation of the joint itself that occurs. This is typically followed by degeneration and arthritis of the great toe joint.</p>
+                    <p> The fundamental driving force behind developing bunions as a result of faulty foot mechanics is genetics. Issues such as loose ligaments and flat feet, are inherited. The traits then act as the foundation for abnormal foot mechanics and lead to formation of bunions. </p>
+                    <p style="padding-top: 25px; text-align: center;"><img src="/images/pathology/bunionangles.jpg" alt="xray of bunion showing abnormal angle" ></p>
+                    <p style="font-size: 12px; text-align: center;">Red line: Abnormal bisection of first metatarsal - Green line: Correct bisection if bone where not splayed out</p>
+                    <div style="padding-top: 25px; text-align: center;"><img src="/images/pathology/BUNIONMORM.jpg" alt="xray of foot with normal angles"><br />
+                    </div>
+                    <p style="text-align: center;">Normal bisection of metatarsal bones</p>
+                    <p>The most common symptoms associated with this condition are pain on the side of the foot just behind the great toe. A red painful bump is usually present. Pain is usually brought on with walking or sports. Shoes don't cause bunions but will typically aggravate them. Stiff leather shoes or shoes with a tapered toe box are the prime offenders. This is why bunion pain is most common in women whose shoes have a pointed toe box. The bunion site will often be slightly swollen and red from the constant rubbing and irritation of a shoe. Occasionally, corns can develop between the 1st and 2nd toe from the pressure the toes rubbing against each other.</p>
+                    
+                </div>
+                
+                
+              <!-- tab -->
+              
+              <div class="tab-pane fade" id="treatment">
+                  <h3>Treatment</h3>
+                    <p>Treatment falls into two broad categories, conservative and surgical. From a conservative standpoint, efforts are directed at correcting faulty foot mechanics with custom molded insoles and relief of symptoms. These include:</p>
+                    <ul>
+                        <li>Custom Orthosis to stabilize the abnormal motion of the hind and fore foot.</li>
+                        <li>Shoe gear modification:  Using shoes with larger toe boxed and more supple materials.</li>
+                        <li>Changes in activities:  Try to avoid those things which cause symptoms.</li>
+                        <li>Anti-inflammatory medication for periodic relief this includes cortisone injections into the joint as well as oral medication.</li>
+                    </ul>
+              </div>
+              
+              <!-- tab -->
+             
+              <div class="tab-pane fade" id="goals">
+                                    <h3>Surgical management:</h3>
+                      <p>Goals of surgery:</p>
+                      <p>The primary goal of bunion surgery is to relieve the pain associated with the deformity. This is accomplished by:</p>
+                      <ul>
+                          <li>correcting the underlying abnormal metatarsal position by realigning it toward the second toe.</li>
+                          <li>Removing excessive bone formation on the bunion "bump"</li>
+                          <li>releasing the soft tissue tightness which is pulling the big toe towards the second toe.</li>
+                          <li>Tightening the soft tissues which are overly stretched on the bump side of the joint.</li>
+                          <li>Reestablish the correct alignment of the cartilage surfaces.</li>
+                          <li>Move the sesamoid bones into correct alignment.</li>
+                          <li>Realign the great toe.</li>
+                      </ul>
+                      <p>Bunion surgery procedures are based on many factors, including health, age and lifestyle of the patient. However, a critical factor in procedure choice is the grading of the bunion deformity.</p>
+                      <p><br />
+                      </p>
+                      <h4>Bunion deformity grading:</h4>
+                      <p>Bunions are grade by the severity into three broad categories:</p>
+                      <ul>
+                          <li><strong>Mild:</strong> Patient's symptoms may include pain and throbbing with activities. There is a slight bump. There is no arthritis of the joint.</li>
+                          <li><strong>Moderate:</strong> Patient's symptoms may include pain and throbbing most days regardless of activity level. Patient has difficulty wearing most styles of shoes without pain. There is a moderate bump. Some arthritis maybe present in the joint. The big toe is pressing firmly against the second toe.</li>
+                          <li><strong>Severe:</strong> Patient's has pain daily. Large bump which is red and irritated from shoes and may deform shoes. Inability to wear all styles of closed shoes without pain. Arthritis is present within the joint. The big toe and second toe may overlap. The big toe cannot be straightened manually.</li>
+                      </ul>
+                      </div>
+                
+                    
+                       
+                        <div class="tab-pane fade" id="moderate">
+                          <h4>Distal Bunion surgery (Distal Bunionectomy):</h4>
+                          
+                            <p>This is the term used to describe surgery which is performed exclusively around the great toe joint. There are several proximal procedures we perform, including "Austin" or chevron procedure and "distal L" or Reverdin-Laird procedure. Indication for a proximal surgery include:</p>
+                            <div style="text-align: center;"><span style="white-space: pre;"><img src="/images/pathology/buniopre.jpg" alt="preop moderate bunion deformity"><br />
+                            </span></div>
+                            <div style="text-align: center;">
+                            <p style="font-size: 12px;">Moderate bunion deformity</p>
+                            </div>
+                            <div>
+                            <ul>
+                                <li>Mild to moderate bunion deformity;</li>
+                                <li>Inability to tolerate proximal procedure;</li>
+                                <li>Minimal to no arthritis within the joint.</li>
+                            </ul>
+                            <h4>Details of distal bunion surgery:</h4>
+                            </div>
+                            <div>
+                            <ul>
+                                <li>Performed under a local anesthetic. You are, however, sedated by an anesthesiologist prior to injection of the numbing agent to your foot.</li>
+                                <li>Incisions are located around the great toe joint only.</li>
+                                <li>Weight bearing is started right after surgery unless instructed otherwise.</li>
+                                <li>Return to work in 1 to 2 weeks if you primarily work in an office setting, sitting most of the day. If you work on your feet throughout the day, such as in heavy industry (wearing steel toed boots, no light duty) you may require 4 to 6 weeks off of work.</li>
+                                <li>Return to low impact cardio (walking, elliptical trainer) in 4-5 weeks.</li>
+                                <li>Return to high impact activities (running, soccer, softball, etc) in 10-15 weeks.</li>
+                                <li>Return to athletic shoes in 1 month, dress shoes without a heel in 2 months, high heels in 3 months.</li>
+                            </ul>
+                            </div>
+                            <div style="text-align: center;"> <iframe width="400" height="233" frameborder="0" src="http://www.youtube.com/embed/NasJwVOXzp0"></iframe> </div>
+                            <div style="text-align: center;"><img src="/images/pathology/bunionpost.jpg" alt="distal bunionectomy postop"></div>
+                            <div style="text-align: center;">
+                            <p style="font-size: 12px;">Example of moderate bunion surgery correction via a distal bunionectomy</p>
+                            </div>
+                            </div>
+                        
+
+                            <!-- tab -->
+
+                       
+                            <div class="tab-pane fade" id="severe">
+                                 <h4>Proximal bunion surgery (proximal bunionectomy):</h4>
+                                      <p>This is the term used to describe surgery which is performed in the around the big toe joint and in the middle part of the foot. Procedures we perform include Lapidus bunionectomy and crecentic bunionectomy. Indications include:</p>
+                                      <div style="text-align: center;"><img src="/images/pathology/lappre.jpg" alt="severe bunion deformity xray" s/></div>
+                                      <div style="text-align: center;">
+                                      <p style="font-size: 12px;">X-ray of a moderate bunion deformity with hypermobility</p>
+                                      </div>
+                                      <ul>
+                                          <li>Moderate to severe deformity;</li>
+                                          <li>Hypermobility (excessive motion) of the metatarsal;</li>
+                                          <li>Rapidly progressing deformity.</li>
+                                      </ul>
+                                      <h4>Details of a proximal bunion surgery:</h4>
+                                      <ul>
+                                          <li>General anesthesia or local anesthesia</li>
+                                          <li>Incisions are located around the great toe joint and the midfoot.</li>
+                                          <li>No weight is allowed on the foot until healing of the fusion site is seen on x-ray (usually 6 weeks);</li>
+                                          <li>Return to work in 2 to 3 weeks if you primarily work in an office setting, sitting most of the day. If you work on your feet throughout the day, such as in heavy industry (wearing steel toed boots, no light duty) you may require 10 to 12 weeks off of work.</li>
+                                          <li>Return to low impact cardio (walking, elliptical trainer) in 8 to 10 weeks.</li>
+                                          <li>Return to high impact activities (running, soccer, softball, etc) in 4 to 6 months</li>
+                                          <li>Return to athletic shoes in 8 to 10 weeks, other style of shoes will depend on extent of pre-operative joint damage.</li>
+                                      </ul>
+                                      <div style="text-align: center;"><img src="/images/pathology/lappost.jpg" alt="postoperative lapidus bunion surgery"> </div>
+                                      <div style="text-align: center;">
+                                      <p style="font-size: 12px;">Lapidus proximal bunion surgery</p>
+                                      </div>
+                                      <ul>
+                                          <li><a href="/treatments-and-conditions/conditions/forefoot/bunions/bunionsurgery.aspx" sfref="[Sitefinity]e039b23b-17c8-4ca4-9348-68b3ce04f345"><strong>Before and After Pictures of Bunion Surgery</strong></a></li>
+                                      </ul>
+                                      </div>
+
+                                    
+                                    
+                                  </div><!-- tab0content --> 
+                        
+
+
+</div><!-- container -->
+</div>
+</div>
+<!-- footer -->
+
+   <?php include '../components/footer.php'; ?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/modernizr.js"></script>
+    <?php include '../../components/social.php'; ?>
+
+
+
+  </body>
+</html>
---
+++ pathology/pathtemplate.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html
+     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+<html>
+  <head>
+    <title>Plantar Fasciitis - Heel Pain</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../../css/bootstrap.css" rel="stylesheet">
+    <!--<link href="css/font-awesome.min.css" rel="stylesheet">-->
+    <link href="../../css/mystyles.css" rel="stylesheet">
+    <!-- <link href="css/glyphicons.css" rel="stylesheet"> -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    
+      <script src="/js/html5shiv.js"></script>
+      <script src="/js/respond.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+  </head>
+
+  <body id="plantarfasciitis">
+
+  <?php include '../components/navhead.php'; ?>
+
+
+
+  <div class="navibar">
+    <section class="container">
+  
+      <div class="pull-left">
+        <p>Bunions<p>
+      </div><!-- pullleft -->
+      <div class="pull-right">
+        <ol class="breadcrumb">
+            <li><a href="../../index.php">Home</a></li>
+            <li><a href="../../pathology.php">Conditions</a></li>
+            <li class="active">Bunions</li>
+        </ol><!-- breadcrumb -->
+      </div><!-- pull-right -->
+     
+    </section><!-- container -->
+  </div><!-- navibar -->
+
+  <!-- end navibar -->
+<div class="layoutspacing2"></div>
+
+<div class="container">
+  
+<ul class="nav nav-tabs nav-justified" style="margin: 20px 0px;">
+              <li class="active"><a href="#cause" data-toggle="tab">Cause</a></li>
+              <li><a href="#signs_symptoms" data-toggle="tab">Signs and Symptoms</a></li>
+              <li><a href="#treatment" data-toggle="tab">Treatment</a></li>
+             
+            </ul>
+
+            <!-- Tab panes -->
+           
+            <div class="tab-content" style="padding: 0px 60px;">
+              <div class="tab-pane fade in active" id="cause">
+
+                </div>
+              <!-- tab -->
+              <div class="tab-pane fade" id="signs_symptoms">
+
+              </div>
+              <!-- tab -->
+              <div class="tab-pane fade" id="treatment">
+
+              </div>
+              </div>
+              
+            </div><!-- tab0content -->
+
+</div><!-- container -->
+
+<!-- footer -->
+
+   <?php include '../components/footer.php'; ?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/modernizr.js"></script>
+    <?php include '../../components/social.php'; ?>
+
+
+
+  </body>
+</html>
