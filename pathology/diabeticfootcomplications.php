f39d45e663363f842868375acfe74b48e410e962
richard.perez <pod1098@richards-iMac.local>
2013-12-03 12:58:12 -0600

    morepathology

---
+++ pathology/diabeticfootcomplications.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html
+     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+<html>
+  <head>
+    <title>Diabetic Foot Complications</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../../css/bootstrap.css" rel="stylesheet">
+    <!--<link href="css/font-awesome.min.css" rel="stylesheet">-->
+    <link href="../../css/mystyles.css" rel="stylesheet">
+    <!-- <link href="css/glyphicons.css" rel="stylesheet"> -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    
+      <script src="/js/html5shiv.js"></script>
+      <script src="/js/respond.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+  </head>
+
+  <body id="plantarfasciitis">
+
+  <?php include '../components/navhead.php'; ?>
+
+
+
+  <div class="navibar">
+    <section class="container">
+  
+      <div class="pull-left">
+        <p>Diabetic Foot Complications<p>
+      </div><!-- pullleft -->
+      <div class="pull-right">
+        <ol class="breadcrumb">
+            <li><a href="../../index.php">Home</a></li>
+            <li><a href="../../pathology.php">Conditions</a></li>
+            <li class="active">Diabetic Foot Complications</li>
+        </ol><!-- breadcrumb -->
+      </div><!-- pull-right -->
+     
+    </section><!-- container -->
+  </div><!-- navibar -->
+
+  <!-- end navibar -->
+<div class="layoutspacing2"></div>
+
+<div class="container">
+  
+<ul class="nav nav-tabs nav-justified" style="margin: 20px 0px;">
+              <li class="active"><a href="#cause" data-toggle="tab">Diabetic Foot Complications</a></li>
+              <li><a href="#signs_symptoms" data-toggle="tab">Prevention</a></li>
+              <li><a href="#treatment" data-toggle="tab">Video</a></li>
+             
+            </ul>
+
+            <!-- Tab panes -->
+           
+            <div class="tab-content" style="padding: 0px 60px;">
+              <div class="tab-pane fade in active" id="cause">
+                  <h5>What are diabetic foot complications?</h5> <p>
+                        Foot ulcers (sores) and infections are a problem that can be caused by diabetes. The severity of the problem can range from minor sores to serious damage of tissues in the foot.</p> <h5>How do they occur?</h5> <p>When you have diabetes, the circulation of blood to your feet is not as good as it should be. It is harder for the body to fight infections and heal itself. As a result, any infections and sores on your feet can become serious. Without treatment, severe infections can cause the flesh of your foot to die (gangrene). Diabetics are 20 times more likely to have gangrene in the foot than people who do not have diabetes.</p> <p>Because diabetes damages nerve endings, you may not feel pain if you hurt your foot or get an infection. This can make it difficult for you to know that you need medical treatment.</p> <h5>What are the symptoms?</h5> <p>The initial symptoms are swelling, redness, or pain. Sores may appear on the sole of your foot. They may heal but later return in the same place. If the sores are not treated, that part of the flesh of your foot may die and turn black.</p> <h5>How are they treated?</h5> <p>You may need antibiotics or other medications to put on the sore or infected part of your foot. The medicine will help fight infection, rid the wound of dead flesh, and help new, healthy flesh to grow.</p> <p>You may have to stay off your feet for a while to prevent further irritation of the sores or infections.</p> <p>You may be hospitalized for treatment. If antibiotics don't heal the infected or ulcerated area, the
+                        doctor may have to remove the infected flesh surgically. If you have gangrene, the doctor may
+                        have to amputate part or all of your foot.</p>
+                </div>
+              <!-- tab -->
+              <div class="tab-pane fade" id="signs_symptoms">
+                        <h5>How can I help prevent diabetic foot complications?</h5> <p>Check your feet every day and follow the foot care guidelines given below.</p> <p>Look at your feet with a mirror at the end of each day to make sure you have no reddened areas,
+                      cuts, or scrapes that could become infected.</p> <p>Bathing and drying:</p> 
+                      <ul> 
+                      <li>Wash your feet every day with soap and warm (not hot) water. Then dry your feet
+                          carefully, especially between the toes. Apply lanolin after your feet are dry to keep the
+                          skin soft and free of scales.</li> 
+                          <li>If your feet sweat a lot, keep them dry by dusting them with talcum powder.</li> 
+                          </ul> 
+                          <p>Treating corns and calluses:</p> 
+                          <ul> 
+                          <li>Don't treat corns or calluses yourself. Do not use over- the-counter products for these
+                          problems unless your health care provider says it is OK.</li> 
+                          <li>Tell your health care provider right away if you develop a corn or callus.</li> </ul> <p>Toenail care:</p> <ul> <li>Cut your toenails carefully. Cut them straight across. Do not cut the sides or the cuticles.</li> 
+                          <li>Clean your nails carefully.</li> 
+                          <li>If your nails are hard to trim, ask your health care provider's office for help.</li> 
+                          </ul> 
+                          <p>Foot warmth:</p> 
+                          <ul> 
+                          <li>Wear cotton socks to bed if you need extra warmth for your feet.</li> 
+                          <li>Avoid using hot water bottles or electric heaters to warm your feet. Because you may not
+                          fully sense hot and cold with your feet, you may burn your feet accidentally and develop
+                          an infection.</li> 
+                          <li>Avoid putting your feet where they could accidentally be burned; for example, on hot
+                          sand at the beach, in hot bath water or whirlpools, or near a fireplace.</li> 
+                          </ul> 
+                          <p>Footwear:</p> 
+                          <ul> 
+                          <li>Wear soft leather shoes that fit properly.</li>
+                           <li>Ask your health care provider about specially made shoes, especially if you have foot
+                          problems.</li> 
+                          <li>Avoid wearing new shoes for more than an hour a day until they are thoroughly broken
+                          in.</li> 
+                          <li>Avoid sitting with your legs crossed.</li> 
+                          <li>Wear clean socks and change them at least once a day.</li> 
+                          </ul> 
+                          <p>In addition to these foot care guidelines, good control of your blood sugar and your blood
+                          pressure help prevent foot problems.</p>
+              </div>
+              <!-- tab -->
+              <div class="tab-pane fade" id="treatment">
+
+              </div>
+              </div>
+              
+            </div><!-- tab0content -->
+
+</div><!-- container -->
+
+<!-- footer -->
+
+   <?php include '../components/footer.php'; ?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/modernizr.js"></script>
+    <?php include '../../components/social.php'; ?>
+
+
+
+  </body>
+</html>
---
+++ pathology/haglunds.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html
+     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+<html>
+  <head>
+    <title>Haglunds Deformity</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../../css/bootstrap.css" rel="stylesheet">
+    <!--<link href="css/font-awesome.min.css" rel="stylesheet">-->
+    <link href="../../css/mystyles.css" rel="stylesheet">
+    <!-- <link href="css/glyphicons.css" rel="stylesheet"> -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    
+      <script src="/js/html5shiv.js"></script>
+      <script src="/js/respond.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+  </head>
+
+  <body id="haglunds">
+
+  <?php include '../components/navhead.php'; ?>
+
+
+
+  <div class="navibar">
+    <section class="container">
+  
+      <div class="pull-left">
+        <p>Haglunds Deformity<p>
+      </div><!-- pullleft -->
+      <div class="pull-right">
+        <ol class="breadcrumb">
+            <li><a href="../../index.php">Home</a></li>
+            <li><a href="../../pathology.php">Conditions</a></li>
+            <li class="active">Haglunds Deformity</li>
+        </ol><!-- breadcrumb -->
+      </div><!-- pull-right -->
+     
+    </section><!-- container -->
+  </div><!-- navibar -->
+
+  <!-- end navibar -->
+<div class="layoutspacing2"></div>
+
+<div class="container">
+  
+<ul class="nav nav-tabs nav-justified" style="margin: 20px 0px;">
+              <li class="active"><a href="#cause" data-toggle="tab">Cause</a></li>
+              <li><a href="#signs_symptoms" data-toggle="tab">Signs and Symptoms</a></li>
+              <li><a href="#treatment" data-toggle="tab">Treatment</a></li>
+             
+            </ul>
+
+            <!-- Tab panes -->
+           
+            <div class="tab-content" style="padding: 0px 60px;">
+              <div class="tab-pane fade in active" id="cause">
+                  <p>Haglund’s deformity is a bony enlargement on the back of the heel that most often leads
+                      to painful bursitis, which is an inflammation of the bursa (a fluid-filled sac between the
+                      tendon and bone). In Haglund’s deformity, the soft tissue near the Achilles tendon
+                      becomes irritated when the bony enlargement rubs against shoes.</p> <p>Haglund’s deformity is often called “pump bump” because the rigid backs of pump-style
+                      shoes can create pressure that aggravates the enlargement when walking. In fact, the
+                      deformity is most common in young women who wear pumps.</p>
+                </div>
+              <!-- tab -->
+              <div class="tab-pane fade" id="signs_symptoms">
+                  <h3>Symptoms</h3> <p>Haglund’s deformity can occur in one or both feet. The signs and symptoms include:</p> <ul> <li>A noticeable bump on the back of heel.</li> <li> Pain in the area where the Achilles tendon attaches to the heel.</li> <li>Swelling in the back of the heel.</li> <li>Redness near the inflamed tissue.</li> </ul> <h3>Causes</h3> <p>To some extent, heredity plays a role in Haglund’s deformity. People can inherit a type
+                  of foot structure that makes them prone to developing this condition.</p> <p>For example, high arches can contribute to Haglund’s deformity. The Achilles tendon
+                  attaches to the back of the heel bone, and in a person with high arches, the heel bone is
+                  tilted backward into the Achilles tendon. This causes the uppermost portion of the back
+                  of the heel bone to rub against the tendon. Eventually, due to this constant irritation, a
+                  bony protrusion develops and the bursa becomes inflamed. It is the inflamed bursa that
+                  produces the redness and swelling associated with Haglund’s deformity.</p> <p>A tight Achilles tendon can also play a role in Haglund’s deformity, causing pain by
+                  compressing the tender and inflamed bursa. In contrast, a tendon that is more flexible
+                  results in less pressure against the painful bursa.</p> <p>Another possible contributor to Haglund’s deformity is a tendency to walk on the outside
+                  of the heel. This tendency, which produces wear on the outer edge of the sole of the
+                  shoe, causes the heel to rotate inward, resulting in a grinding of the heel bone against
+                  the tendon. The tendon protects itself by forming a bursa, which eventually becomes
+                  inflamed and tender.</p> <h3>Diagnosis</h3> <p>Radiographs, ultrasound and MRI along with a physical signs and symptoms are utilized to diagnose the condition.</p>
+              </div>
+             <!-- tab -->
+              <div class="tab-pane fade" id="treatment">
+                  <h3>Treatment</h3> <p>Non-surgical treatment of Haglund’s deformity is aimed at reducing the inflammation of
+                    the bursa. While these approaches can resolve the bursitis, they will not shrink the bony
+                    protrusion. Non-surgical treatment can include medication, icing, stretching, lifts,
+                    orthosis, physical therapy and extra corporeal <a title="shockwave treatment" href="/treatments-and-conditions/shockwave-treatment.aspx">shockwave treatment</a>.&nbsp;
+                    If non-surgical treatment fails to provide adequate pain relief, surgery may be needed.</p> <p>A variety of surgical techniques can be utilized to restore function</p>
+              </div>
+              </div>
+              
+            </div><!-- tab0content -->
+
+</div><!-- container -->
+
+<!-- footer -->
+
+   <?php include '../components/footer.php'; ?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/modernizr.js"></script>
+    <?php include '../../components/social.php'; ?>
+
+
+
+  </body>
+</html>
