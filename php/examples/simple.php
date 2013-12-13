a4570c6e85eb2b07101a7dd5ce51f72c86fac99a
richard.perez <pod1098@richards-iMac.local>
2013-12-13 16:33:57 -0600

    wowmoreforms

--- php/examples/simple.php
+++ php/examples/simple.php
@@ -28,11 +28,11 @@
     try
     {
         $response = $email_service->queue($payload);
-        printf("Mail successfully sent! ID = %s", $response->result);
+        printf('<a href="../podlink.com/index.php">Mail</a> successfully sent! ID = %s', $response->result);
     }
     catch(ServiceException $exception)
     {
         printf("Error! %s (%s)", $exception->getMessage(), $exception->getErrorCode());
     }
-    header("Location: http://www.podlink.herokuapp.com/index.php");
+
 ?>
