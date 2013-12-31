2a0592a6852adfa14311eccca5ddcf67fdc4da34
richard.perez <pod1098@richards-iMac.local>
2013-12-31 15:29:10 -0600

    title

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 
-<!DOCTYPE html
-     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <!DOCTYPE html
+   PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 
-<html lang="en-US"itemscope itemtype="http://schema.org/podiatric">
+  <html lang="en-US"itemscope itemtype="http://schema.org/podiatric">
   <head>
     <title>Alamo Family Foot & Ankle Care San Antonio's - Most Trusted Podiatrists</title>
     <meta name="description" content="As the premiere podiatrist in San Antonio, Alamo Family Foot & Ankle Care offers services from three state of the art Podiatry clinics in the San Antonio area.  Our board certified doctors offer a combined sixty-one years of practice experience to the patient community."/>
@@ -12,33 +12,34 @@
     <!-- bing authorization -->
     <meta name="msvalidate.01" content="FA44635B9B76597E752EE0BFB60B89F0" />
     <meta name="google-site-verification" content="8pkl5tuBhGKs_XFheIJ8pmXyBeExSoQiuVZhCT1Acv4" />
+
     <!-- end bing authorization -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    
+
     <meta property="twitter:account_id" content=""/>
-        <link rel="canonical" href=""/>
-        <!-- fb -->
+    <link rel="canonical" href=""/>
+    <!-- fb -->
 
-        <meta name="og:title" content="Alamo Family Foot & Ankle Care San Antonio's - Most Trusted Podiatrist"/>
-        <meta name="og:url" content="podlink.com"/>
-        <meta name="og:description" content="As the premiere podiatrist in San Antonio, Alamo Family Foot & Ankle Care offers services from three state of the art Podiatry clinics in the San Antonio area.  Our board certified doctors offer a combined sixty-one years of practice experience to the patient community."/>
-        <meta name="og:image" content=""/>
+    <meta name="og:title" content="Alamo Family Foot & Ankle Care San Antonio's - Most Trusted Podiatrist"/>
+    <meta name="og:url" content="podlink.com"/>
+    <meta name="og:description" content="As the premiere podiatrist in San Antonio, Alamo Family Foot & Ankle Care offers services from three state of the art Podiatry clinics in the San Antonio area.  Our board certified doctors offer a combined sixty-one years of practice experience to the patient community."/>
+    <meta name="og:image" content=""/>
 
-        <!-- twitter -->
-       
-        <!-- Google+ -->
-        <meta itemprop="name" content="Alamo Family Foot & Ankle Care San Antonio's most trusted podiatrist"/>
-        <meta itemprop="description" content="As the premiere podiatrist in San Antonio, we offer services from three state of the art Podiatry clinics in the San Antonio area.  Our board certified doctors offer a combined sixty-one years of practice experience to the patient community."/>
-        <meta itemprop="audience" content="Patients"/>
-                <meta itemprop="image" content=""/>
-                        <meta itemprop="alternativeHeadline" content="Alamo Family Foot and Ankle Care"/>
-                
-              
-                <link rel="author" href="http://plus.google.com/113774021362941412953" />
-        
-        <!--<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"> -->
-       <!-- <link rel="icon" href="/favicon.ico" type="image/x-icon"> -->
+    <!-- twitter -->
+
+    <!-- Google+ -->
+    <meta itemprop="name" content="Alamo Family Foot & Ankle Care San Antonio's most trusted podiatrist"/>
+    <meta itemprop="description" content="As the premiere podiatrist in San Antonio, we offer services from three state of the art Podiatry clinics in the San Antonio area.  Our board certified doctors offer a combined sixty-one years of practice experience to the patient community."/>
+    <meta itemprop="audience" content="Patients"/>
+    <meta itemprop="image" content=""/>
+    <meta itemprop="alternativeHeadline" content="Alamo Family Foot and Ankle Care"/>
+
+
+    <link rel="author" href="http://plus.google.com/113774021362941412953" />
+
+    <!--<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"> -->
+    <!-- <link rel="icon" href="/favicon.ico" type="image/x-icon"> -->
 
 
 
@@ -51,8 +52,8 @@
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    
-      
+
+
     <meta http-equiv="X-UA-Compatible" content="IE=9">
 
 
@@ -67,215 +68,246 @@
 
 
 
-  <?php include 'components/navhead.php'; ?>
-
-    
-
-   
-  <?php include 'components/carousel.php'; ?>
+    <?php include 'components/navhead.php'; ?>
 
 
 
-<section class="toolbar hidden-xs">
 
-  <div class="container">
-    <section class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-    
-    <div id="tools">
-      <ul class="nav nav-justified">
-         
-          <li><a href="/resources/npforms.php"><span class="glyphicons-icon white notes_2"></span>New Patient Forms</a></li>
-          
-          <li> <a href="location.php"><span class="glyphicons-icon white compass"></span>Locations</a></li>
-          <li><a href="contact.php"><span class="glyphicons-icon white headset"></span>Contact</a></li>
-          
-        </ul>
-
-    </div> <!-- tools -->
-    </div><!-- col -->
-    </section><!-- row -->
-  </div><!-- container--> 
-
-</section> <!-- toolbar -->
+    <?php include 'components/carousel.php'; ?>
 
 
-<section class="toolbar visible-xs">
 
-  <div class="container">
-    <section class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-    
-    <div id="tools">
-      <ul class="nav nav-justified">
-         
-          <li><a href="#" data-toggle="modal" data-target="#gps"><span class="glyphicons-icon white google_maps"></span>GPS Directions</a></li>
-          
-          <li class="#"><a href="location.php"><span class="glyphicons-icon white compass"></span>Locations</a></li>
-          <li><a href="contact.php"><span class="glyphicons-icon white headset"></span>Contact</a></li>
-          
-        </ul>
-        
-        <!-- modal for selecting office -->
-        <?php include 'components/gpsmodal.php'; ?>
+    <section class="toolbar hidden-xs">
 
+      <div class="container">
+        <section class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
-    </div> <!-- tools -->
-    </div><!-- col -->
-    </section><!-- row -->
-  </div><!-- container--> 
+            <div id="tools">
+              <ul class="nav nav-justified">
 
-</section> <!-- toolbar -->
+                <li><a href="/resources/npforms.php"><span class="glyphicons-icon white notes_2"></span>New Patient Forms</a></li>
+
+                <li> <a href="location.php"><span class="glyphicons-icon white compass"></span>Locations</a></li>
+                <li><a href="contact.php"><span class="glyphicons-icon white headset"></span>Contact</a></li>
+
+              </ul>
+
+            </div> <!-- tools -->
+          </div><!-- col -->
+        </section><!-- row -->
+      </div><!-- container--> 
+
+    </section> <!-- toolbar -->
 
 
-<div id="bomb"></div>
+    <section class="toolbar visible-xs">
+
+      <div class="container">
+        <section class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+            <div id="tools">
+              <ul class="nav nav-justified">
+
+                <li><a href="#" data-toggle="modal" data-target="#gps"><span class="glyphicons-icon white google_maps"></span>GPS Directions</a></li>
+
+                <li class="#"><a href="location.php"><span class="glyphicons-icon white compass"></span>Locations</a></li>
+                <li><a href="contact.php"><span class="glyphicons-icon white headset"></span>Contact</a></li>
+
+              </ul>
+
+              <!-- modal for selecting office -->
+              <?php include 'components/gpsmodal.php'; ?>
 
 
-     
+            </div> <!-- tools -->
+          </div><!-- col -->
+        </section><!-- row -->
+      </div><!-- container--> 
+
+    </section> <!-- toolbar -->
+
+
+    <div id="bomb"></div>
+
+
+
 
     <section class="container">
       <div id="bumperpads">
-     
 
-<!-- col 1 -->
+
+        <!-- col 1 -->
         <section class="col col-lg-4">
-          
+
           <div class="center">
             <section class="enlarge">
               <span class="glyphicons lightbulb"></span>
             </section> <!-- enlarge -->
-        
-          <h2>Knowledge</h2>
-          <hr>
-   
-        <p>We provide comprehensive state of the art  medical and surgical care for a wide variety of foot and ankle conditions.  Our board certified physicians are well trained to diagnose and treat common to complex disorders and injuries that affect the foot and ankle in people of all ages.</p>
-             </div> <!-- center-->
-   
-    </section><!-- col col-lg --><!-- col 1 -->
+            
+            <h2>Knowledge</h2>
+            <hr>
 
-<!-- col 2 -->
-    <section class="col col-lg-4">
-        <div class="center">
-          <section class="enlarge">
+            <p>We provide comprehensive state of the art  medical and surgical care for a wide variety of foot and ankle conditions.  Our board certified physicians are well trained to diagnose and treat common to complex disorders and injuries that affect the foot and ankle in people of all ages.</p>
+          </div> <!-- center-->
+
+        </section><!-- col col-lg --><!-- col 1 -->
+
+        <!-- col 2 -->
+        <section class="col col-lg-4">
+          <div class="center">
+            <section class="enlarge">
               <span class="glyphicons cogwheels"></span>
             </section> <!-- enlarge -->
-        
-          <h2>Experience</h2>
-          <hr>
-          
-        <h7>As the premiere podiatry practice in San Antonio, Alamo Family Foot & Ankle Care offers services from three state of the art Podiatry clinics in the San Antonio area.  Our physicians and surgeons offer a combined sixty-one years of practice experience to the patient community. </h7>
-      </div> <!-- center--> 
+            
+            <h2>Experience</h2>
+            <hr>
 
-    </section><!-- col col-lg -->
- 
-<!-- col 3 -->
+            <h7>As the premiere podiatry practice in San Antonio, Alamo Family Foot & Ankle Care offers services from three state of the art Podiatry clinics in the San Antonio area.  Our physicians and surgeons offer a combined sixty-one years of practice experience to the patient community. </h7>
+          </div> <!-- center--> 
 
-    <section class="col col-lg-4">
-        <div class="center">
-          <section class="enlarge">
+        </section><!-- col col-lg -->
+
+        <!-- col 3 -->
+
+        <section class="col col-lg-4">
+          <div class="center">
+            <section class="enlarge">
               <span class="glyphicons screenshot"></span>
             </section> <!-- enlarge -->
-        
-          <h2>Results</h2>
-          <hr>
-          
-        <p>Our Doctors utilize the best available well established, proven scientific research to deliver evidence based medicine(EBM) to our patients.  By emphasiving EBM and applying treatments backed by scientific studies, patients can achieve superior outcomes. <p>
-      </div> <!-- center-->
-      
-    </section><!-- col col-lg -->
+            
+            <h2>Results</h2>
+            <hr>
 
-    </div><!-- bumperpads -->
+            <p>Our Doctors utilize the best available well established, proven scientific research to deliver evidence based medicine(EBM) to our patients.  By emphasiving EBM and applying treatments backed by scientific studies, patients can achieve superior outcomes. <p>
+            </div> <!-- center-->
 
-  </section> <!-- container -->
+          </section><!-- col col-lg -->
 
+        </div><!-- bumperpads -->
 
-
-</section><!-- container -->
-</section>
+      </section> <!-- container -->
 
 
 
+    </section><!-- container -->
+  </section>
 
 
- <section class="container">
- <hr>
-        <div class="lead col-lg-6 center"> 
-            <div class="layoutspacing2"></div>
-            <p>We consistently have satisfied patients.  In our most recent patient survey, 91% of our patients "strongly agreed" or "agreed" with the statement, "I was satisfied with my medical care."  </p>      
-        </div><!-- lead -->
+
+
+
+  <section class="container">
+   <hr>
+   <div class="lead col-lg-6 center"> 
+    <div class="layoutspacing2"></div>
+    <p>We consistently have satisfied patients.  In our most recent patient survey, 91% of our patients "strongly agreed" or "agreed" with the statement, "I was satisfied with my medical care."  </p>      
+  </div><!-- lead -->
   <div class="col-lg-6 hidden-xs">
-        <div id="piechart" style="height: 400px; width: 700px; margin-top: 0px;"> </div><!-- piechart -->
-        
-    </div><!-- col -->
+    <div id="piechart" style="height: 400px; width: 700px; margin-top: 0px;"> </div><!-- piechart -->
+
+  </div><!-- col -->
 
   <div class="visible-xs">
-        <div id="piechart" style=""> </div><!-- piechart -->
-        
-    </div><!-- col -->  
+    <div id="piechart" style=""> </div><!-- piechart -->
 
-</section>  <!-- container -->     
-  
+  </div><!-- col -->  
+
+  </section>  <!-- container -->     
+
   <?php include 'components/carouselReviews.php'; ?>
 
 
 
-<div class="layoutspacing2">
-  <div class="container"><hr></div>
-</div>
+  <div class="layoutspacing2">
+    <div class="container"><hr></div>
+  </div>
+
+  <?php include 'components/picturelineup.php'; ?>
+
+  <div class="layoutspacing2"></div>  
+
+  <div class="container"><hr></div> 
+
+  <div class="container">
+      <section class="center">
+      <section class="iconsizing">
+              <span class="glyphicons hospital"></span>
+      </section> <!-- container -->
+    </section><!-- center -->
+     <div class="layoutspacing2"> </div>
+    <h2 class="center">What is the training and education of Podiatrist?</h2>
+
+    <div class="col-lg-7" style="padding-top:50px">
+
+       
+        <iframe id="ytplayer" type="text/html" width="640" height="360"
+        src="https://www.youtube.com/embed/CQ8ak79KCWQ?modestbranding=1&rel=0&showinfo=0&theme=light"
+        frameborder="0" allowfullscreen></iframe>
+      
+    </div><!-- col-lg-8 -->
+
+    <div class="col-lg-5 lead">
+    As Podiatrist and Foot and Ankle Surgeons, we treat any condition of the foot and ankle 
+      using traditional, proven literature, driven by evidence based medicine.  Podiatrist are truly specialized in what we do. We spend four years in medical school learning about the whole body like every other medical student. Podiatrist then enter into residency where we work side by side with clinicians from internal medicine, general surgery, emergency medicine, endocrinology and radiology among others to form a solid foundation in advance of entering private practice.
+
+
+
+      
+    </div><!-- col-lg-4 -->
     
-             <?php include 'components/picturelineup.php'; ?>
 
-  <div class="layoutspacing2"></div>        
+  </div><!-- container -->
 
-<!-- footer -->
+<div class="layoutspacing2"></div>  
 
-  
+  <!-- footer -->
+
+
   <?php include 'components/footer.php'; ?>        
-          
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/modernizr.js"></script>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
 
 
-
-  
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://code.jquery.com/jquery.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/modernizr.js"></script>
+  <script src="js/html5shiv.js"></script>
+  <script src="js/respond.min.js"></script>
 
 
 
 
-    <!-- google charts script -->
-     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Strongly Agree / Agree',     140],
-          
-          ['Neutral',  9],
-          ['Disagree', 4]
+
+
+
+
+  <!-- google charts script -->
+  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript">
+    google.load("visualization", "1", {packages:["corechart"]});
+    google.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Strongly Agree / Agree',     140],
+
+        ['Neutral',  9],
+        ['Disagree', 4]
 
         ]);
 
-        var options = {
-          title: '"I was satisfied with my medical care"'
-        };
+      var options = {
+        title: '"I was satisfied with my medical care"'
+      };
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-        chart.draw(data, options);
-      }
-     </script>
-     <!-- end google charts script -->
-
-     <script>
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+      chart.draw(data, options);
+    }
+  </script>
+  <!-- end google charts script -->
 
 
-</script> 
   </body>
-</html>
+  </html>
