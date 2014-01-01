4f1ac30939a5ceb76cd2a1606d33a417fef6f6d1
richard.perez <pod1098@richards-iMac.local>
2014-01-01 17:41:25 -0600

    more

--- components/navhead.php
+++ components/navhead.php
@@ -89,7 +89,7 @@
                     </form>
 
 !-- google analytics -->
-script>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -98,7 +98,7 @@ script>
   ga('create', 'UA-18705174-1', 'podlink.com');
   ga('send', 'pageview');
 
-/script>
+</script>
 <!-- google analytics -->
 
 
