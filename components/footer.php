897e9d48fb417228c2fdfab498ef0f14e0ee599f
richard.perez <pod1098@richards-iMac.local>
2014-01-02 17:22:14 -0600

    record

--- components/footer.php
+++ components/footer.php
@@ -90,4 +90,20 @@
 		</div><!-- navbarfixedbottom -->
 	</section><!-- footer div -->
 <?php include  $_SERVER['DOCUMENT_ROOT'] . '/components/social.php'; ?>
+<!-- ClickTale Bottom part -->
+
+<script type='text/javascript'>
+// The ClickTale Balkan Tracking Code may be programmatically customized using hooks:
+// 
+//   function ClickTalePreRecordingHook() { /* place your customized code here */  }
+//
+// For details about ClickTale hooks, please consult the wiki page http://wiki.clicktale.com/Article/Customizing_code_version_2
+
+document.write(unescape("%3Cscript%20src='"+
+(document.location.protocol=='https:'?
+"https://clicktalecdn.sslcs.cdngc.net/www02/ptc/27cdc7b6-6f15-48a0-b81d-8d3947d038b3.js":
+"http://cdn.clicktale.net/www02/ptc/27cdc7b6-6f15-48a0-b81d-8d3947d038b3.js")+"'%20type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+<!-- ClickTale end of Bottom part -->
 </footer><!-- footer -->
