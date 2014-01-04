ebf114baf99842505408267e3e8b821406d4e2de
richard.perez <pod1098@richards-iMac.local>
2014-01-04 11:41:57 -0600

    fix

--- components/navhead.php
+++ components/navhead.php
@@ -99,7 +99,7 @@ var WRInitTime=(new Date()).getTime();
 
 !-- google analytics -->
 
-script>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -108,7 +108,7 @@ script>
   ga('create', 'UA-18705174-1', 'podlink.com');
   ga('send', 'pageview');
 
-/script>
+</script>
 <!-- google analytics -->
 
 
