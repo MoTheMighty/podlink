5f43dd750cc912ca54e7b4e492461582c3788e72
richard.perez <pod1098@richards-iMac.attlocal.net>
2015-06-21 18:12:11 -0500

    fb code

--- components/footer.php
+++ components/footer.php
@@ -82,7 +82,24 @@
 
 </script>
 
-
+<!-- facebook script -->
+<script>(function() {
+var _fbq = window._fbq || (window._fbq = []);
+if (!_fbq.loaded) {
+var fbds = document.createElement('script');
+fbds.async = true;
+fbds.src = '//connect.facebook.net/en_US/fbds.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(fbds, s);
+_fbq.loaded = true;
+}
+_fbq.push(['addPixelId', '435021813349136']);
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', 'PixelInitialized', {}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=435021813349136&amp;ev=PixelInitialized" /></noscript>
+<!-- end facebook script -->
 
 
 </footer><!-- footer -->
